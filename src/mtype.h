9e78b999dd4ff7b357183ba5ca490c16533d4103
Joshua Chin <joshuarchin@gmail.com>
2021-09-04 11:05:54 -0400

    Add support for partial armor coverage for monsters (#51304)
    * Modify load_damage_array to specify default
    
    * Add weakpoint.h
    
    * Implement weakpoint.cpp
    
    * Various compiler fixes
    
    * Add weakpoints to mtype.
    
    * Let weakpoints modify monster armor.
    
    * Compiler fixes
    
    * weakpoint bugfixes.
    
    * Run astyle
    
    * Add json documentation
    
    * Run json_formatter
    
    * Run clang tidy
    
    * Fix GCC-only warning
    
    * clang-tidy
    
    * Clarify weakpoint docs and switch to percentage coverage.
    
    * Rename armor offset to armor penalty, flipping the sign.

--- src/mtype.h
+++ src/mtype.h
@@ -23,6 +23,7 @@
 #include "translations.h"
 #include "type_id.h"
 #include "units.h" // IWYU pragma: keep
+#include "weakpoint.h"
 
 class Creature;
 class monster;
@@ -332,6 +333,7 @@ struct mtype {
         int armor_bullet = -1;  /** innate armor vs. bullet */
         int armor_acid = -1;    /** innate armor vs. acid */
         int armor_fire = -1;    /** innate armor vs. fire */
+        ::weakpoints weakpoints;
 
         // Bleed rate in percent, 0 makes the monster immune to bleeding
         int bleed_rate = 100;
